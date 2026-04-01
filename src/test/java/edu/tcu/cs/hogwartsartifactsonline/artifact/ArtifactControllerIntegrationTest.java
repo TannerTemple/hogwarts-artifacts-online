516e487490406e186cdff25146e36b379b6bd885
Tanner Temple <tannerjtemple@gmail.com>
2026-04-01 14:37:31 -0500

    User authentication and authorization done! Close #5.

---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactControllerIntegrationTest.java
@@ -0,0 +1,95 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Integration tests for Artifact API endpoints")
+@Tag("integration")
+public class ArtifactControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    String token;
+
+    @Value("${api.endpoint.base-url}")
+    String baseUrl;
+
+    @BeforeEach
+    void setup() throws Exception {
+        ResultActions resultActions = this.mockMvc
+                .perform(post(this.baseUrl + "/users/login")
+                        .with(httpBasic("john", "123456")));
+        MvcResult mvcResult = resultActions.andDo(print()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+       JSONObject json = new JSONObject(contentAsString);
+       this.token = "Bearer " + json.getJSONObject("data").getString("token"); //Don't forget to add "Bearer " as a prefix
+    }
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    void testFindAllArtifactsSuccess() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get(this.baseUrl + "/artifacts"). accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Find All Success"))
+                .andExpect(jsonPath("$.data", Matchers.hasSize(6)));
+    }
+
+    @Test
+    @DisplayName("Check addArtifact with valid input (POST)")
+//    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    void testAddArtifactSuccess() throws Exception {
+        Artifact a = new Artifact();
+        a.setName("Remembrall");
+        a.setDescription("A Remembrall was a magical large marble-sized glass ball that contained smoke which turned red when its owner or user had forgotten something. It turned clear once whatever was forgotten was remembered.");
+        a.setImageUrl("ImageUrl");
+
+        String json = this.objectMapper.writeValueAsString(a);
+
+        this.mockMvc.perform(post(this.baseUrl + "/artifacts").contentType(MediaType.APPLICATION_JSON).content(json).accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, this.token))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Add Success"))
+                .andExpect(jsonPath("$.data.id").isNotEmpty())
+                .andExpect(jsonPath("$.data.name").value("Remembrall"))
+                .andExpect(jsonPath("$.data.description").value("A Remembrall was a magical large marble-sized glass ball that contained smoke which turned red when its owner or user had forgotten something. It turned clear once whatever was forgotten was remembered."))
+                .andExpect(jsonPath("$.data.imageUrl").value("ImageUrl"));
+        this.mockMvc.perform(MockMvcRequestBuilders.get(this.baseUrl + "/artifacts").accept(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, this.token))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Find All Success"))
+                .andExpect(jsonPath("$.data", Matchers.hasSize(7)));
+    }
+
+
+}
